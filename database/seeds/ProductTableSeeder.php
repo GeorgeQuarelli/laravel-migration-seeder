2f3c331540013b649f0fb9ebf45f9443685c6f01
George <quarelli.george@gmail.com>
2020-07-12 17:57:32 +0200

    fakers

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Product;
+Use Faker\Generator as Faker;
 
 class ProductTableSeeder extends Seeder
 {
@@ -10,28 +11,36 @@ class ProductTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $dati_prodotti = [
-            [
-                'nome' => 'iPhone 11 Pro',
-                'price' => '1600.00',
-            ],
-            [
-                'nome' => 'iPhone 11 Pro Max',
-                'price' => '1200.00',
-            ],
-            [
-                'nome' => 'iPhone 11',
-                'price' => '890.00',
-            ]
-        ];
+        // $dati_prodotti = [
+        //     [
+        //         'nome' => 'iPhone 11 Pro',
+        //         'price' => '1600.00',
+        //     ],
+        //     [
+        //         'nome' => 'iPhone 11 Pro Max',
+        //         'price' => '1200.00',
+        //     ],
+        //     [
+        //         'nome' => 'iPhone 11',
+        //         'price' => '890.00',
+        //     ]
+        // ];
+        //
+        // foreach ($dati_prodotti as $dati_prodotto) {
+        //     $nuovo_prodotto = new Product();
+        //     $nuovo_prodotto->name=$dati_prodotto['nome'];
+        //     $nuovo_prodotto->description='iphone description';
+        //     $nuovo_prodotto->price=$dati_prodotto['price'];
+        //     $nuovo_prodotto->save();
+        // }
 
-        foreach ($dati_prodotti as $dati_prodotto) {
+        for ($i = 0; $i < 10; $i++) {
             $nuovo_prodotto = new Product();
-            $nuovo_prodotto->name=$dati_prodotto['nome'];
-            $nuovo_prodotto->description='iphone description';
-            $nuovo_prodotto->price=$dati_prodotto['price'];
+            $nuovo_prodotto->name= $faker->sentence(4);
+            $nuovo_prodotto->description= $faker->text(50);
+            $nuovo_prodotto->price= $faker->randomFloat(2, 10, 1000);
             $nuovo_prodotto->save();
         }
     }
